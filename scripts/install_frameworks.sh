5d0ac71b0b32f3b7d17470623a0e20a20eef2e58
Curtis Doty <Curtis@GreenKey.net>
2023-06-02 09:03:14 -0700

    Paring down CI deps

--- scripts/install_frameworks.sh
+++ scripts/install_frameworks.sh
@@ -2,14 +2,15 @@
 set -ev
 
 if [ "$DJANGO_VERSION" ]
-then pip install Django==$DJANGO_VERSION
+then
+    pip install Django==$DJANGO_VERSION
 fi
 
 if [ "$FLASK_VERSION" ]
 then
-    pip install itsdangerous==2.0.1
-    pip install Jinja2==3.0.3
-    pip install werkzeug==2.0.3
+    # pip install itsdangerous==2.0.1
+    # pip install Jinja2==3.0.3
+    # pip install werkzeug==2.0.3
     pip install Flask==$FLASK_VERSION
 fi
 
