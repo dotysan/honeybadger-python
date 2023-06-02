1be7add7f9450622334f68319c2220c6810f8ee8
Curtis Doty <Curtis@GreenKey.net>
2023-06-02 08:36:40 -0700

    Move hard-version Flask deps back in

--- scripts/install_frameworks.sh
+++ scripts/install_frameworks.sh
@@ -6,7 +6,11 @@ then pip install Django==$DJANGO_VERSION
 fi
 
 if [ "$FLASK_VERSION" ]
-then pip install Flask==$FLASK_VERSION
+then
+    pip install itsdangerous==2.0.1
+    pip install Jinja2==3.0.3
+    pip install werkzeug==2.0.3
+    pip install Flask==$FLASK_VERSION
 fi
 
 echo "OK"
